8e7b8041c4dbe02fd3c9b148c2178dee6a5e716a
jerzy <egornik62@gmail.com>
2024-02-08 22:53:34 +0300

    add axios

--- src/services/GetData.ts
+++ src/services/GetData.ts
@@ -1,13 +1,24 @@
 import { useEffect, useState } from 'react'
 import { IPeople } from './models'
+import axios from 'axios'
 
 export const GetData = () => {
   const [data, setData] = useState<IPeople[]>([])
 
   useEffect(() => {
-    fetch('https://swapi.dev/api/people/')
-      .then((data) => {
-        return data.json()
+    // fetch('https://swapi.dev/api/people/')
+    //   .then((data) => {
+    //     return data.json()
+    //   })
+    //   .then((info) => {
+    //     console.log(info.results)
+    //     setData(info.results)
+    //   })
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then((response) => {
+        console.log(response.data)
+        return response.data
       })
       .then((info) => {
         console.log(info.results)
